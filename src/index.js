332a4f1fe42fcffcbc01b1f8b476bbcfca7f8c85
marleypm16 <yarlempm16@gmail.com>
2024-04-26 22:10:09 -0300

    Finish index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 const buttonOpenMenu = document.querySelector('.button-open-menu');
+const image = document.querySelector('.image');
 const buttonCloseMenu = document.querySelector('.button-close-menu');
+
+function trocarSrc() {
+    if (window.innerWidth > 1023) {
+        image.src = '../assets/image-web-3-desktop.jpg'; // Defina o novo caminho da imagem
+    } else {
+        image.src = '../assets/image-web-3-mobile.jpg'; // Reverta para o caminho original se a largura da tela for menor ou igual a 1023 pixels
+    }
+}
+
+// Execute a função inicialmente para definir o src com base na largura da tela
+trocarSrc();
+
+// Adicione um listener de redimensionamento para atualizar o src quando a largura da tela mudar
+window.addEventListener('resize', trocarSrc);
+
 buttonOpenMenu.addEventListener('click', () => {
     console.log('click');
     const aside = document.querySelector('.aside');
